9a0582c5687d057dc0bc4accf7893e9f647ed1d0
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-01 11:31:35 +0530

    Method 2 Runnable Interface

--- src/RunnableInterface.java
+++ src/RunnableInterface.java
@@ -8,7 +8,7 @@ public class RunnableInterface implements Runnable{
     public static void main(String[] args) {
         
         RunnableInterface r1 = new RunnableInterface();
-        Thread t1 = new Thread(r1;)
+        Thread t1 = new Thread(r1);
         t1.start();
     }
     
