aa1d9dd7c507e5b35e7c9683e1ba7e21c69291ff
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-01 11:27:17 +0530

    Method 1 Extending Thread Class

--- src/ExtendingThreadClass.java
+++ src/ExtendingThreadClass.java
@@ -1,6 +1,12 @@
 public class ExtendingThreadClass  extends Thread{
+
+    public void run()
+    {
+        System.out.println("Thred is running...");
+    }
     
     public static void main(String[] args) {
         
+       
     }
 }
