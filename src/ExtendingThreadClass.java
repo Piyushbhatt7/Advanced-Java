0d205f5f1aaf0945da067b0dc718d567d937c31f
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-03 22:50:19 +0530

    Extending Thread Class in java

--- src/ExtendingThreadClass.java
+++ src/ExtendingThreadClass.java
@@ -7,7 +7,7 @@ public class ExtendingThreadClass  extends Thread{
     
     public static void main(String[] args) {
         
-       ExtendingThreadClass t1 = new ExtendingThreadClass();
+       ExtendingThreadClass t1 = new ExtendingThreadClass(); 
        t1.start();
     }
 }
