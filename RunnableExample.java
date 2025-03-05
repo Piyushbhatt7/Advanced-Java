76c9a87f98f1df73458ccdc0376471e2a678b5d4
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-05 20:12:58 +0530

    Runnable Interface

--- RunnableExample.java
+++ RunnableExample.java
@@ -15,14 +15,14 @@ class MyRunnable2 implements Runnable {
 }
 
 
-public class Runnable {
+public class RunnableExample {
     
     public static void main(String[] args) {
         MyRunnable r1 = new MyRunnable();
         MyRunnable2 r2 = new MyRunnable2();
 
-        Thread t1 = new Thread(r1);
-        Thread t2 = new Thread(r2);
+        // Thread t1 = new Thread(r1);
+        // Thread t2 = new Thread(r2);
 
         t1.start();
         t2.start();
