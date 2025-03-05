1d7b18b74660e762b978ee615813f56c7c8e3f98
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-05 23:32:37 +0530

    Runnable example 1

--- RunnableExample.java
+++ RunnableExample.java
@@ -18,7 +18,7 @@ class MyRunnable2 implements Runnable {
     @Override
     public void run()
     {
-        int i = 0;
+        int i = 0; 
         while(i <= 20)
         {
             System.out.println("I am a thread 2");
