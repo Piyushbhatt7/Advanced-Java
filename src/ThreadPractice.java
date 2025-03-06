e034a758c3ff49ed73dc494aa4bc7ca0beaa1b03
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-06 23:17:29 +0530

    Thread Practice

--- src/ThreadPractice.java
+++ src/ThreadPractice.java
@@ -11,11 +11,26 @@ class Practice extends Thread {
     }
 }
 
+class Practice2 extends Thread {
 
+    public void run()
+    {
+        int i = 0;
+        while(i < 20)
+        {
+            System.out.println("Welcome");
+            i++;
+        }
+    }
+}
 
 public class ThreadPractice {
     
     public static void main(String[] args) {
         
+        Practice t1 = new Practice();
+        Practice2 t2 = new Practice2();
+        t1.start();
+        t2.start();
     }
 }
