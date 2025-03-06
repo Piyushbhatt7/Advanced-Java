6d7effc79bb8759a6b022dc147f28a60c71a996d
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-06 21:52:53 +0530

    Java Thread Priorities

--- src/ThreadPriorities.java
+++ src/ThreadPriorities.java
@@ -21,9 +21,9 @@ public class ThreadPriorities {
         
         // Ready Queue : T1 T2 T3 T4 T5
         MyThread t1 = new MyThread("Harry");
-        MyThread t2 = new MyThread("Harry");
-        MyThread t3 = new MyThread("Harry");
-        MyThread t4 = new MyThread("Harry");
-        MyThread t5 = new MyThread("Harry");
+        MyThread t2 = new MyThread("Don");
+        MyThread t3 = new MyThread("Badmosh");
+        MyThread t4 = new MyThread("Chintu");
+        MyThread t5 = new MyThread("Savan");
     }
 }
