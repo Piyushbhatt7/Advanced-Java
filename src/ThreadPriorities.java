060e6f6b0d3433bc3e57720170b97fc200609d00
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-06 21:45:56 +0530

    Java Thread Priorities

--- src/ThreadPriorities.java
+++ src/ThreadPriorities.java
@@ -2,6 +2,6 @@ public class ThreadPriorities {
     
     public static void main(String[] args) {
         
-        // Ready Queue : T1 T2, T3, T4, T5
+        // Ready Queue : T1 T2 T3, T4, T5
     }
 }
