9a6891734e21a41590e6c4c29a7793d7452dd781
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-06 22:00:15 +0530

    Java Thread Priorities

--- src/ThreadPriorities.java
+++ src/ThreadPriorities.java
@@ -27,6 +27,7 @@ public class ThreadPriorities {
         MyThread t4 = new MyThread("Chintu (most Important)");
         MyThread t5 = new MyThread("Savan");
         t4.setPriority(Thread.MAX_PRIORITY);
+        t1.setPriority(Thread.MIN_PRIORITY);
         t1.start();
         t2.start();
         t3.start();
