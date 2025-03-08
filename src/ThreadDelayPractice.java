fb2cc091bf4913f5b2160a65358d5c9984cae008
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-08 11:20:25 +0530

    Thread Delay Practice

--- src/ThreadDelayPractice.java
+++ src/ThreadDelayPractice.java
@@ -1,3 +1,33 @@
+class Practice extends Thread {
+
+    @Override
+    public void run()
+    { 
+        int i = 0;
+        while(i < 20)  
+        {
+            System.out.println("Good Morning");
+            i++;
+        } 
+    }                    
+}
+
+class Practice2 extends Thread {
+
+    @Override
+    public void run()
+    { 
+        int i = 0;
+        while(i < 20)
+        {
+            System.out.println("Welcome");
+            i++;
+        }
+    }
+}
+
+
+
 public class ThreadDelayPractice {
     
     public static void main(String[] args) {
