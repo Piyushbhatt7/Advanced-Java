8314530ceb180202ac2059f9a826db5ab1ac1dff
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-20 13:18:37 +0530

    packages in java

--- Piyush.java
+++ Piyush.java
@@ -4,5 +4,6 @@ public class Piyush {
     public static void main(String[] args) {
         
         System.out.println("package -> piyush");
+        java.util.Scanner sc = new java.util.Scanner(System.in);
     }
 }
