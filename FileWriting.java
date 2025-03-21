b2cec7d1eb4acf003da9965d3222bb471debe248
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-21 23:22:40 +0530

    writing in a file

--- FileWriting.java
+++ FileWriting.java
@@ -1,10 +1,18 @@
 import java.io.File;
+import java.io.FileWriter;
 
 public class FileWriting {
     
     public static void main(String[] args) {
         
         File obj = new File("wite.txt");
-        
+        try {
+            
+            FileWriter writer = new FileWriter(obj);
+            writer.write("Pussy boy");
+        } 
+        catch (Exception e) {
+            System.out.println("An error occured");
+        }
     }
 }
