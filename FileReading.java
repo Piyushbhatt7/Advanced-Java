625113c9393aa78d54264a71a99d78a7cd17771c
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-21 23:31:32 +0530

    File Reading in java

--- FileReading.java
+++ FileReading.java
@@ -1,7 +1,27 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class FileReading{
 
     public static void main(String[] args) {
         
-        
+        File obj = new File("wite.txt");
+
+        try{
+            Scanner sc = new Scanner(obj);
+            while(sc.hasNext())
+            {
+                String line = sc.next();
+                System.out.println(line);
+            }
+        }
+        catch(FileNotFoundException e)
+        {
+            System.out.println("error to read a file");
+        }
+       
+
     }
 }
