2e703d67cf8c9f8c615fc6d399623db0bfd4bd6f
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-21 23:18:20 +0530

    creating a file

--- CreatingAFile.java
+++ CreatingAFile.java
@@ -1,7 +1,11 @@
+
+import java.io.File;
+
 public class CreatingAFile {
     
     public static void main(String[] args) {
         
-        File myfile = 
+        File myfile = new File("hii.txt");
+        
     }
 }
