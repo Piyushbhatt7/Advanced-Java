a3728f97a5816a07112599f5aa9de1c75753ad2d
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-21 18:41:58 +0530

    file handling in java

--- filehandling.java
+++ filehandling.java
@@ -1,6 +1,11 @@
+
+import java.io.File;
+
 public class FileHandling {
     
     public static void main(String[] args) {
         
+        File myFile = new File("file.txt");
+        myFile.createNewFile();
     }
 }
