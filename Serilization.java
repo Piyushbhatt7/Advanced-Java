e51384cfef3223d28d76910ed501ace10d2dcbf0
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-21 21:32:27 +0530

    Serilization

--- Serilization.java
+++ Serilization.java
@@ -8,7 +8,13 @@ class Student implements Serializable {
 
     public Student(String name, int age)
     {
-        
+        this.name = name;
+        this.age = age;
+    }
+
+    public void display()
+    {
+        System.out.println("Name: "+ name + "Age: " + age);
     }
 }
 
