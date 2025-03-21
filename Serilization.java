a792ca840038677b78c4cd417e49a3421c3d8408
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-21 21:31:02 +0530

    Serilization

--- Serilization.java
+++ Serilization.java
@@ -1,6 +1,17 @@
 
 import java.io.Serializable;
 
+class Student implements Serializable {
+
+    String name;
+    int age;
+
+    public Student(String name, int age)
+    {
+        
+    }
+}
+
 public class Serilization implements Serializable{
     
     public static void main(String[] args) {
