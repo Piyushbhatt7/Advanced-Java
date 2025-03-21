90ffb92e4e5a12684998cc7cee34293139f49104
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-21 23:19:46 +0530

    creating a file

--- CreatingAFile.java
+++ CreatingAFile.java
@@ -1,11 +1,19 @@
 
 import java.io.File;
+import java.io.IOException;
 
 public class CreatingAFile {
     
     public static void main(String[] args) {
         
         File myfile = new File("hii.txt");
-        
+        try {
+            myfile.createNewFile();
+            System.out.println("File created successfully");
+        } 
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+      
     }
 }
