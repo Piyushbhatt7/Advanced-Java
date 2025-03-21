96e57f911b1f98c67b49ce2e2312f61f6e11cb86
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-21 18:43:07 +0530

    file handling in java

--- filehandling.java
+++ filehandling.java
@@ -1,11 +1,17 @@
 
 import java.io.File;
+import java.io.IOException;
 
 public class FileHandling {
     
     public static void main(String[] args) {
         
-        File myFile = new File("file.txt");
-        myFile.createNewFile();
+        try {
+            File myFile = new File("file.txt");
+            myFile.createNewFile();
+        } 
+        catch (IOException ex) {
+            System.out.println("Enable to cretye file");
+        }
     }
 }
