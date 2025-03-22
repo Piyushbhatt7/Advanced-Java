a4e452882e86c2bbb72e6beb58bb1f8b05b5c7a3
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-22 22:19:26 +0530

    File Input Stream

--- src/InputStream.java
+++ src/InputStream.java
@@ -1,7 +1,25 @@
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class InputStream {
     
     public static void main(String[] args) {
         
-        
+        try(FileInputStream is = new FileInputStream("ex.txt"))
+        {
+            int i;
+            while((i = is.read()) != -1)
+            {
+                System.out.println((char)i);
+            }
+
+        }
+
+        catch(IOException e)
+        {
+            System.out.println(e.getMessage());
+
+        }
     }
 }
