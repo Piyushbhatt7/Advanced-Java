a4b1c29b7f83962238bdf995e25ce55bca7cea6c
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-22 23:17:39 +0530

    Buffer Input Stream

--- src/BufferInputStream.java
+++ src/BufferInputStream.java
@@ -1,6 +1,13 @@
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 public class BufferInputStream {
     
     public static void main(String[] args) {
         
+        try(BufferInputStream bis = new BufferInputStream(new FileInputStream("ex.txt"));
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("ex.txt");)
+        )
     }
 }
