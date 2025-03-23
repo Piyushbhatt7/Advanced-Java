8c6ad0a797719f12cb0f5a3564ecbd9a545b8c75
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-23 14:57:18 +0530

    Throw Practice keyword in java

--- ThrowEx.java
+++ ThrowEx.java
@@ -2,5 +2,10 @@ public class ThrowEx {
     
     public static void main(String[] args) {
         
+        int age = 15;
+        if(age < 18)
+        {
+            throw new IllegalArgumentException("Not eligible to vote");
+        }
     }
 }
