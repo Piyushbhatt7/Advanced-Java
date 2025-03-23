c07a03982076584d4165321503ffe9fccf883522
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-23 23:14:26 +0530

    Thread Priorities

--- src/ThreadPrioritiesEx.java
+++ src/ThreadPrioritiesEx.java
@@ -21,6 +21,10 @@ public class ThreadPrioritiesEx {
         MyThtr t2 = new MyThtr("Subham");
         MyThtr t3 = new MyThtr("Kunal");
 
+        t3.setPriority(10);
+        t2.setPriority(1);
+        t1.setPriority(5);
+
         t1.start();
         t2.start();
         t3.start();
