ed82bde605c995095a61fb8cdc51d37a7a0d777b
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-23 23:10:13 +0530

    Thread Priorities

--- src/ThreadPrioritiesEx.java
+++ src/ThreadPrioritiesEx.java
@@ -1,6 +1,23 @@
+class MyThtr extends Thread {
+
+    public MyThtr(String name)
+    {
+        super(name);
+    }
+
+    public void run()
+    {
+        
+            System.out.println("I am a thread");
+        
+    }
+}
+
 public class ThreadPrioritiesEx {
     
     public static void main(String[] args) {
         
+        MyThtr t1 = new MyThtr("Aman");
+        MyThtr t2 = new MyThtr("Subham");
     }
 }
