cfa7608c92e26921cf2548cad6735974b2435c53
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-23 14:39:48 +0530

    Throws keyword in java

--- throws.java
+++ throws.java
@@ -1,6 +1,14 @@
 public class Throws {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ArithmeticException{
         
+        int a = 10;
+        int b = 0;
+        int result = a/b;
+
+        try{
+            System.out.println("Result" + result);
+        }
+
     }
 }
