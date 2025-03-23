3054a2ea95cd8ea7d9866de5be0a600717b174e7
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-23 14:52:08 +0530

    Throws Practice keyword in java

--- ThrowsEx.java
+++ ThrowsEx.java
@@ -1,6 +1,17 @@
 public class ThrowsEx {
     
+    
+    public static int divide(int a, int b) throws ArithmeticException{
+        int result = a/b;
+        return result;
+    }
+    
     public static void main(String[] args) {
         
+        try {
+            
+        } catch (Exception e) {
+        }
+       
     }
 }
