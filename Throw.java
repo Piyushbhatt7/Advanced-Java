2a9c1b080a19db4762c52701619299d1f7d1604f
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-23 14:43:44 +0530

    Throw keyword in java

--- Throw.java
+++ Throw.java
@@ -2,6 +2,8 @@ public class Throw {
     
     public static void main(String[] args) {
         
-        
+        int a =10, b = 0;
+       int c = a / b;
+        System.out.println();
     }
 }
