6e01f20ce9973cbc99b6eb94cbba8166da114e05
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-23 23:19:56 +0530

    Thread Methods in java

--- src/ThreadMethodsEx.java
+++ src/ThreadMethodsEx.java
@@ -1,3 +1,18 @@
+class MyThtrM extends Thread {
+
+    public MyThtrM(String name)
+    {
+        super(name);
+    }
+
+    public void run()
+    {
+        
+            System.out.println("I am a thread");
+        
+    }
+}
+
 public class ThreadMethodsEx {
     
     public static void main(String[] args) {
