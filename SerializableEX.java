8b55bf2b770993698289b6fb08965c9e1548fc48
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-24 00:14:23 +0530

    class Serializable in java

--- SerializableEX.java
+++ SerializableEX.java
@@ -7,7 +7,11 @@ class student implements Serializable
     String name;
     int age;
     
-    
+    public student(String name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
 }
 
 public class SerializableEX {
