f702aea4aa1dda95332489a2218f9f85105594ca
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-24 11:21:01 +0530

    Dead Lock in java

--- DeadlockDemo.java
+++ DeadlockDemo.java
@@ -19,5 +19,20 @@ public class DeadlockDemo {
                 System.out.println("Thread 1 Aquired lock2");
             }
         });
+
+        Thread t2 = new Thread(()  -> {
+            synchronized (lock2)
+            {
+                System.out.println("Thread2 Holding lock1");
+            }
+            try{Thread.sleep(100);}
+            catch(InterruptedException e){}
+            System.out.println("Thread 2 waiting for lock2");
+            synchronized (lock1)
+            {
+                System.out.println("Thread 2 Aquired lock2");
+            }
+        });
+        
     }
 }
