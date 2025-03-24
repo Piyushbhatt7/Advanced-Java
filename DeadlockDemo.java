7f6bd352ec036ff11d2eb7a9ce7236ead26b218f
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-24 11:15:20 +0530

    Dead Lock in java

--- DeadlockDemo.java
+++ DeadlockDemo.java
@@ -1,4 +1,9 @@
 public class DeadlockDemo {
+
+    private static final Object lock1 = new Object();
+    private static final Object lock2() = new Object();
+    
+
     public static void main(String[] args) {
         
     }
