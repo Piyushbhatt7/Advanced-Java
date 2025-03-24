b83d75831c94679e85f69133737ec456e3c3920b
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-24 11:06:13 +0530

    DeadLock in java

--- DeadLock.java
+++ DeadLock.java
@@ -1,6 +1,29 @@
+
+
 public class DeadLock {
     
     public static void main(String[] args) {
         
+        void ri()
+        {
+            synchronized(lock1)
+            {
+                synchronized(lock2)
+                {
+                    System.out.println("Hii");
+                }
+            }
+        }
+
+        void r2()
+        {
+            synchronized(lock2)
+            {
+                synchronized(lock1)
+                {
+                    System.out.println("World");
+                }
+            }
+        }
     }
 }
