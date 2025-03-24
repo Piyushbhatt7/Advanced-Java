666f00e83d6b34c50b1f1fa8c8b83e8153608dd6
Piyushbhatt7 <vengersmon003@gmail.com>
2025-03-24 11:16:28 +0530

    Dead Lock in java

--- DeadlockDemo.java
+++ DeadlockDemo.java
@@ -1,10 +1,16 @@
 public class DeadlockDemo {
 
     private static final Object lock1 = new Object();
-    private static final Object lock2() = new Object();
-    
+    private static final Object lock2 = new Object();
+
 
     public static void main(String[] args) {
         
+        Thread t1 = new Thread(() -> {
+            synchronized (lock1)
+            {
+                System.out.println("Thread1 Holding lock1");
+            }
+        });
     }
 }
