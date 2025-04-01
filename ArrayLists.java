5502556c38a5b5534172f009e70589c8635a0eaf
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-01 11:10:07 +0530

    Array List

---
+++ ArrayLists.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class ArrayLists {
+    
+    public static void main(String[] args) {
+
+       //  Integer | Float | Boolean class
+        ArrayList<Integer> list = new ArrayList<Integer>();
+    }
+}
