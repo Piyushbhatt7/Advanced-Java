dedbcfafe987d282293469f78426361927ddfc0f
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-01 11:12:06 +0530

    Array List

--- ArrayLists.java
+++ ArrayLists.java
@@ -6,7 +6,15 @@ public class ArrayLists {
 
        //  Integer | Float | Boolean class
         ArrayList<Integer> list = new ArrayList<Integer>();
-        ArrayList<String> list1 = new ArrayList<String>();
-        ArrayList<Boolean> list2 = new ArrayList<Boolean>();
+        // ArrayList<String> list1 = new ArrayList<String>();
+        // ArrayList<Boolean> list2 = new ArrayList<Boolean>();
+
+        // add element
+
+        list.add(0);
+        list.add(1);
+        list.add(2);
+
+        System.out.println(list);
     }
 }
