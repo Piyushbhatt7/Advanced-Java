7b649a1146177143ac4cbdbc1a512003d1345d66
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-01 10:33:07 +0530

    Merge sort

--- MergeSort.java
+++ MergeSort.java
@@ -1,6 +1,6 @@
 public class MergeSort {
     
-    public static void conquer(int arr[], int mid, int si, int ei)
+    public static void conquer(int arr[], int si, int mid, int ei)
     {
         int merged[] = new int[ei - si + 1];
 
@@ -41,13 +41,14 @@ public class MergeSort {
         return;
     }
      int mid = si + (ei - si) / 2;
-     divide(arr, si, ei);
+     divide(arr, si, mid);
      divide(arr, mid + 1, ei);
+     conquer(arr, si, mid, ei);
    }
 
     public static void main(String args[])
     {
-        int arr[] = {6,2,9,5,2,8};
+        int arr[] = {6,2,9,5,3,8};
         int n = arr.length;
 
         divide(arr, 0, n-1);
