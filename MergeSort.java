1af68111c6cd1cf1ccac02e50f6ba09e52c90365
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-01 10:11:53 +0530

    Merge sort

--- MergeSort.java
+++ MergeSort.java
@@ -1,6 +1,9 @@
 public class MergeSort {
     
-
+   public static void divide(int arr[], int si, int ei)
+   {
+    mid = si + (ei - si) / 2;
+   }
 
     public static void main(String args[])
     {
