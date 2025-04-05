67e4d194362a3efe160a159d90a586361af8ee66
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-05 11:11:17 +0530

    Linked list example with basic code

--- LinkedListEx1.java
+++ LinkedListEx1.java
@@ -1,7 +1,13 @@
 public class LinkedListEx1 {
+
+    class Node {
+
+        String data;
+        Node next;
+    }
     
     public static void main(String[] args) {
         
-        
+
     }
 }
