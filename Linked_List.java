8c8f3319fb411eeb6a6cdd023af45bc142951fa1
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-05 13:40:30 +0530

    creating a linked list

---
+++ Linked_List.java
@@ -0,0 +1,13 @@
+
+import java.util.LinkedList;
+
+public class Linked_List {
+    
+    public static void main(String[] args) {
+        
+        LinkedList<String> list = new LinkedList<>();
+
+        list.addFirst("a");
+        list.addFirst("is");
+    }
+}
