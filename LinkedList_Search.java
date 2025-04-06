d1fa8d44dbb9edc36bb09285d6adc725dab3fc5b
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-06 13:11:35 +0530

    searching an element in Linked list

--- LinkedList_Search.java
+++ LinkedList_Search.java
@@ -1,3 +1,10 @@
+import java.util.LinkedList;
+
 public class LinkedList_Search {
     
+    public static void main(String[] args) {
+        
+        LinkedList<int> list = new LinkedList<>();
+        
+    }
 }
