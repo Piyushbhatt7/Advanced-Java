8baf03529831eb0f8673c3a20f97ad1b8e2fd31e
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-06 19:44:47 +0530

    reversing the LinkedList elements

--- LinkedList_Reverse.java
+++ LinkedList_Reverse.java
@@ -1,6 +1,86 @@
 public class LinkedList_Reverse {
     
+    Node head;
+
+    class Node {
+
+        String data;
+        Node next;
+
+        Node(String data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // add - first, add- last
+    public void addFirst(String data)
+    {
+        Node newNode = new Node(data);
+        if(head == null)
+        {
+           head = newNode;
+           return;
+        }
+
+        newNode.next = head;
+        head = newNode;
+
+    }
+
+    // add-last
+
+    public void addLast(String data)
+    {
+        Node newNode = new Node(data);
+        if(head == null)
+        {
+           head = newNode;
+           return;
+        }
+
+        Node currNode = head;
+        while(currNode.next != null)
+        {
+            currNode = currNode.next;
+        }
+
+        currNode.next = newNode;
+    }
+
+    // print
+
+    public void printList()
+    {
+        if(head == null)
+        {
+            System.out.println("List is empty");
+            return;
+        }
+        Node currNode = head;
+        while(currNode != null)
+        {
+            System.out.print(currNode.data + " -> ");
+            currNode = currNode.next;
+        }
+        
+        System.out.println("Null");
+    }
+
+    
     public static void main(String[] args) {
+        
+        LinkedListEx1 list = new LinkedListEx1();
+        list.addFirst("a");
+        list.addFirst("is");
+        list.printList();
+
+        list.addLast("linked list");
+        list.printList();
+
+        list.addFirst("this");
+        list.printList();
         
     }
 }
