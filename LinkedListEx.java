7aa751852ee089db9c409f77301df174241df8c3
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-06 13:56:03 +0530

    Linked List as input from the user and deleting nodes > 25

--- LinkedListEx.java
+++ LinkedListEx.java
@@ -1,10 +1,13 @@
 
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class LinkedListEx {
     
     public static void main(String[] args) {
         
         LinkedList<Integer> list = new LinkedList<>();
+        Scanner sc = new Scanner(System.in);
+        
     }
 }
