92fedcf1fdad8136840615900461e89ad17f56a9
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-06 13:56:54 +0530

    Linked List as input from the user and deleting nodes > 25

--- LinkedListEx.java
+++ LinkedListEx.java
@@ -7,7 +7,9 @@ public class LinkedListEx {
     public static void main(String[] args) {
         
         LinkedList<Integer> list = new LinkedList<>();
+        System.out.println("Enter number between 1-50");
         Scanner sc = new Scanner(System.in);
-        
+        int num = sc.nextInt();
+
     }
 }
