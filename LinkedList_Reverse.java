53ae71909396c7d4af3e8866ec33e4b257d13e71
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-07 23:38:00 +0530

    reversing the elements of linked list

--- LinkedList_Reverse.java
+++ LinkedList_Reverse.java
@@ -111,11 +111,11 @@ public class LinkedList_Reverse {
         
         LinkedList_Reverse list = new LinkedList_Reverse();
         list.addFirst(1);
-        // list.addLast(2);
-        // list.addLast(3);
-        // list.addLast(4); 
+        list.addLast(2);
+        list.addLast(3);
+        list.addLast(4); 
         list.printList();
-// 13 - 17
+
         list.head = list.rverseResursive(list.head);
         list.printList();
         
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,30 +1,3 @@
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Scanner;
-
-public class Collection_LinkedList_Reverse {
-    
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        LinkedList<Integer> list = new LinkedList<>();
-        // list.add(1);
-        // list.add(2);
-        // list.add(3);
-        // list.add(4);
-        // list.add(5);
-
-        System.out.println("Enter the numbers: ");
-        int n = sc.nextInt();
-        
-        for(int i = 0; i < n; i++){
-            int value = sc.nextInt();
-            list.add(value);
-        }
-
-        System.out.print("Original List: " + list);
-
-        Collections.reverse(list);
-        System.out.print(list);
-        sc.close();
-    }
-}
+list.addLast(2);
+        // list.addLast(3);
+        // list.addLast(4);
