b5417bb713c4f3c9728e3af57255bfa335b5bf89
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-07 13:56:38 +0530

    Using collection framework reversing the LinkedList elements

--- Collection_LinkedList_Reverse.java
+++ Collection_LinkedList_Reverse.java
@@ -5,6 +5,10 @@ public class Collection_LinkedList_Reverse {
     public static void main(String[] args) {
         
         LinkedList<Integer> list = new LinkedList<>();
-        
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
     }
 }
