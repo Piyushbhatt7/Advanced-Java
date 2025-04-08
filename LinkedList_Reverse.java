d16007e7397454b30f10df35c500181b91896c29
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-08 21:31:03 +0530

    reversing the linked list elements

--- LinkedList_Reverse.java
+++ LinkedList_Reverse.java
@@ -113,7 +113,7 @@ public class LinkedList_Reverse {
         list.addFirst(1);
         list.addLast(2);
         list.addLast(3);
-        list.addLast(4);        
+        list.addLast(4);         
         list.printList();
 
         list.head = list.rverseResursive(list.head);
