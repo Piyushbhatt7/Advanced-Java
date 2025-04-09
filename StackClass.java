babfe8af94f0af7d0120ee2afaffd53f2d5bc975
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-09 08:12:29 +0530

    implementation of stack in java

--- StackClass.java
+++ StackClass.java
@@ -1,3 +1,12 @@
 public class StackClass {
     
+    static class Node {
+        int data;
+        Node next;
+        public Node(int data)
+        {
+            this.data = data;
+            next = null;
+        }
+    }
 }
