3e977f9537599d9c4209debf1ffed7266f77f42b
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-09 19:42:25 +0530

    creating a stack using ArrayList

--- ArrayList_Stack.java
+++ ArrayList_Stack.java
@@ -5,7 +5,7 @@ public class ArrayList_Stack {
     class Stack {
         ArrayList<Integer> list = new ArrayList<>();
         public boolean isEmpty(){
-            list.size() == 0;
+          return  list.size() == 0;
         }
     }
 }
