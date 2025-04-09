2f15dcae2b4bc47af1a95b2e551e9d0ab01754e7
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-09 19:52:45 +0530

    creating a stack using ArrayList

--- ArrayList_Stack.java
+++ ArrayList_Stack.java
@@ -42,5 +42,10 @@ public class ArrayList_Stack {
         s.push(2);
         s.push(3);
         s.push(4);
+
+        while (s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
     }
 }
