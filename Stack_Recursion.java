afb077eb827b05c080917c86e3b0d971620a9810
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-09 22:02:40 +0530

    stack implementation using recusion

--- Stack_Recursion.java
+++ Stack_Recursion.java
@@ -1,10 +1,18 @@
+
+import java.util.Stack;
+
 public class Stack_Recursion {
     
     public static void pushAtButtom(int data)
     {
-        
+
     }
     public static void main(String[] args) {
         
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
     }
 }
