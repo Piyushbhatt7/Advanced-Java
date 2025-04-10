041bdee641c58c6f3fbfaa41c68dd7230bce1db5
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-10 23:06:41 +0530

    Queue in java

--- QueueP.java
+++ QueueP.java
@@ -68,7 +68,7 @@ public class QueueP {
        q.add(2);
        q.add(3); 
        q.add(4);
-       q.add(5);                 
+       q.add(5);                  
 
        while(!q.isEmpty())
        {
