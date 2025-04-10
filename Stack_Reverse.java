13bd4883a9e67921d9f69c878f18ba915e86871e
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-10 09:23:57 +0530

    Stack reverse

--- Stack_Reverse.java
+++ Stack_Reverse.java
@@ -1,4 +1,31 @@
+
+import java.util.Stack;
+
 public class Stack_Reverse{
 
-    
+    public static void pushAtButtom(int data, Stack<Integer> stack)
+    {
+        if(stack.isEmpty())
+        {
+            stack.push(data);
+            return;
+        }
+       int top = stack.pop();
+       pushAtButtom(data, stack);
+       stack.push(top);
+    }
+    public static void main(String[] args) {
+        
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        Stack_Recursion.pushAtButtom(4, stack);
+
+        while(!stack.isEmpty()) 
+        {
+            System.out.println(stack.peek());
+            stack.pop();
+        }
+    }
 }
