31f3774cc5626ca5b86f3c23b778cfc03d821235
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-11 22:46:55 +0530

    Queue implementation

--- QueueP.java
+++ QueueP.java
@@ -63,8 +63,8 @@ public class QueueP {
  
         // peek
         public static int peek()  
-        { 
-            if(isEmpty())  
+        {  
+            if(isEmpty())     
             {
                 System.out.println("empty queue");    
                 return -1;        
