892dcf42826d84a913e74d9df61d9a13e2e5d3cb
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-12 22:43:55 +0530

    Implementing stack using recursion

--- Stack_Recursion.java
+++ Stack_Recursion.java
@@ -12,7 +12,7 @@ import java.util.Stack;
        stack.push(top); 
     }          
     public static void main(String[] args) {
-        
+          
         Stack<Integer> stack = new Stack<>();
         stack.push(1);
         stack.push(2);     
