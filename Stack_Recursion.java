94e4d3d087da4df1ace34a2e7aaf50c56676be20
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-12 22:43:00 +0530

    Implementing stack using recursion

--- Stack_Recursion.java
+++ Stack_Recursion.java
@@ -1,9 +1,7 @@
 
 import java.util.Stack;
- 
-public class Stack_Recursion {
-    
-    public static void pushAtButtom(int data, Stack<Integer> stack)
+ public class Stack_Recursion {
+        public static void pushAtButtom(int data, Stack<Integer> stack)
     {
         if(stack.isEmpty())   
         {
