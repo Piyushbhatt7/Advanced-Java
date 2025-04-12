fa2cd3681f1b65e34bfd1809884ddcd140606493
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-12 22:49:07 +0530

    Serilization in java

--- Serilized.java
+++ Serilized.java
@@ -3,15 +3,13 @@ import java.io.Serializable;
 
 public class Serilized implements Serializable{
     
-    public static void main(String[] args) {
-        
         String name;
         int age;
 
-        public student(String name, int age)
+        public Student(String name, int age)
         {
             this.name = name;
             this.age = age;
         }
-    }
+    
 }
