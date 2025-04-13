92aeff7a0ad06562da27d5914c049f73628e0a47
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-13 22:31:40 +0530

    Implementing queue using collection framework

--- QueueFramework.java
+++ QueueFramework.java
@@ -6,7 +6,7 @@ public class QueueFramework {
         Queue<Integer> q = new LinkedList<>();
         q.add(1);
         q.add(2);
-        q.add(3);
+        q.add(3); 
         q.add(4);      
         q.add(4);        
         q.add(5); 
