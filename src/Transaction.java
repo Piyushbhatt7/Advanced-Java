520708f7958bf346ed22ee815f912f7b09216d2f
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-01 22:34:32 +0530

    Transaction using JDBC

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,7 +1,15 @@
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
 public class Transaction {
     
     public static void main(String[] args) {
         
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/your_database", "username", "password");
+        } catch (Exception e) {
+        }
     }
 }
