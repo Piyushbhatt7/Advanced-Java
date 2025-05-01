52c43a835d26f5a3eacc5b77fe4867e0b758e8a1
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-01 21:35:02 +0530

    Runnable Class

--- RunnableClass.java
+++ RunnableClass.java
@@ -2,7 +2,7 @@ public class RunnableClass implements Runnable{
 
     public void run(){
         for (int i = 0; i < 10; i++) {
-            
+            System.out.println("Runnable thread");
         }
     }
 
