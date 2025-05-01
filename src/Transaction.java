0a795d27f08312fd3fdf2181007d9ea4df9a7d02
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-01 22:37:13 +0530

    Transaction using JDBC

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,7 +1,7 @@
 
-import java.beans.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 
 
 public class Transaction {
@@ -12,7 +12,10 @@ public class Transaction {
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/your_database", "username", "password");
             conn.setAutoCommit(false);
 
-            Statement stm = conn.createStatement();
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate("INSERT INTO accounts VALUES (1, 'John', 5000)");
+            stmt.executeUpdate("UPDATE accounts SET balance = balance - 1000 WHERE id = 1");
+
         }
          catch (Exception e) {
 
