901d5f70e805966017f8a7f68f258244b926756e
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-01 21:34:41 +0530

    Runnable Class

--- RunnableClass.java
+++ RunnableClass.java
@@ -1,4 +1,11 @@
 public class RunnableClass implements Runnable{
+
+    public void run(){
+        for (int i = 0; i < 10; i++) {
+            
+        }
+    }
+
     
     public static void main(String[] args) {
         
