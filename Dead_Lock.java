c4b3bad85a261c2b62268da2d5e406c65da14152
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-02 22:46:07 +0530

    Dead Lock

--- Dead_Lock.java
+++ Dead_Lock.java
@@ -1,34 +1,37 @@
 public class Dead_Lock {
-    
+
+    private final Object lock1 = new Object();
+    private final Object lock2 = new Object();
+
+    public void method1() {
+        synchronized (lock1) {
+            System.out.println("Thread 1: Holding lock1...");
+            try { Thread.sleep(100); } catch (InterruptedException e) {}
+
+            synchronized (lock2) {
+                System.out.println("Thread 1: Holding lock1 & lock2...");
+            }
+        }
+    }
+
+    public void method2() {
+        synchronized (lock2) {
+            System.out.println("Thread 2: Holding lock2...");
+            try { Thread.sleep(100); } catch (InterruptedException e) {}
+
+            synchronized (lock1) {
+                System.out.println("Thread 2: Holding lock2 & lock1...");
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        
-        private final Object lock1 = new Object();
-        private final Object lock2 = new Object();
+        Dead_Lock deadlock = new Dead_Lock();
 
-        public void method1(){
-            synchronized(lock1){
-                synchronized(lock2)
-                {
-                    // Critical section
-                    System.out.println("Thread 1: Holding lock1 and lock2...");
-                    try { Thread.sleep(100); } catch (InterruptedException e) {}
-                    System.out.println("Thread 1: Waiting for lock2...");
-                }
-            }
-        }
-      
-        public void method2()
-        {
-            synchronized(lock2)
-            {
-                synchronized(lock1)
-                {
-                    System.out.println("Thread 1: Holding lock1 and lock2...");
-                    try { Thread.sleep(100); } catch (InterruptedException e) {}
-                    System.out.println("Thread 1: Waiting for lock2...");
-                }
-            }
-        }
+        Thread t1 = new Thread(() -> deadlock.method1());
+        Thread t2 = new Thread(() -> deadlock.method2());
 
+        t1.start();
+        t2.start();
     }
 }
