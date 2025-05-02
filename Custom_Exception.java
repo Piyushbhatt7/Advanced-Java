2da5670f42e8020db2d8e9dfe63cac8e414b6a91
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-02 10:24:57 +0530

    Custom Exception

--- Custom_Exception.java
+++ Custom_Exception.java
@@ -1,3 +1,13 @@
+class CheckedException extends Exception{
+
+    public CheckedException(String message)
+    {
+        
+    }
+}
+
+
+
 public class Custom_Exception {
     
     public static void main(String[] args) {
