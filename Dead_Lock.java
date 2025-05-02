2a924c2ad0beae11ed098df9aae8f7f25bc1bb7c
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-02 22:26:53 +0530

    Dead Lock

--- Dead_Lock.java
+++ Dead_Lock.java
@@ -5,6 +5,25 @@ public class Dead_Lock {
         private final Object lock1 = new Object();
         private final Object lock2 = new Object();
 
-        
+        public void method1(){
+            synchronized(lock1){
+                synchronized(lock2)
+                {
+
+                }
+            }
+        }
+      
+        public void method2()
+        {
+            synchronized(lock2)
+            {
+                synchronized(lock1)
+                {
+                    
+                }
+            }
+        }
+
     }
 }
