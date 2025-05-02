8a1a629d73e7ffd98307962933020fc33526519a
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-02 15:28:12 +0530

    Serilization and Desilization in java

--- SerDe.java
+++ SerDe.java
@@ -1,6 +1,8 @@
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
@@ -32,6 +34,25 @@ public class SerDe {
             Student obj = new Student("Piyush", 10000000);
             out.writeObject(obj);
             System.out.println("Serilization done...");
+        }
+        catch(IOException e)
+        {
+            System.out.println(e);
+        }
+
+
+        try(ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename)))
+        {
+            Student obj1;
+            try {
+                obj1 = (Student) in.readObject();
+                obj1.display();
+                System.out.println("Desirilization done..");
+            } catch (ClassNotFoundException ex) {
+                System.out.println(ex);
+            }
+            
+            
         }
     }
 
