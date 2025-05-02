33761c0769f5096ade68f35e71f5892816c1f53a
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-02 21:53:55 +0530

    Thread Priority

--- Thread_Priority.java
+++ Thread_Priority.java
@@ -2,5 +2,11 @@ public class Thread_Priority extends Thread {
 
     public static void main(String[] args) {
         
+        Thread_Priority t1 = new Thread_Priority();
+        Thread_Priority t2 = new Thread_Priority();
+        
+        t1.setPriority(MAX_PRIORITY);
+        t2.setPriority(MIN_PRIORITY);
+        
     }
 }
