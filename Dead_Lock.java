b96b0f77c84ec61a669659f9181de6efc7a208fa
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-02 22:04:48 +0530

    Dead Lock

--- Dead_Lock.java
+++ Dead_Lock.java
@@ -3,6 +3,8 @@ public class Dead_Lock {
     public static void main(String[] args) {
         
         private final Object lock1 = new Object();
+        private final Object lock2 = new Object();
+
         
     }
 }
