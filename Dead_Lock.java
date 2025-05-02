8967572940b0c6e6a71390c2b19001e4dce33486
Piyushbhatt7 <vengersmon003@gmail.com>
2025-05-02 22:50:05 +0530

    Dead_Lock

--- Dead_Lock.java
+++ Dead_Lock.java
@@ -1,29 +1,7 @@
 public class Dead_Lock {
 
-    private final Object lock1 = new Object();
-    private final Object lock2 = new Object();
-
-    public void method1() {
-        synchronized (lock1) {
-            System.out.println("Thread 1: Holding lock1...");
-            try { Thread.sleep(100); } catch (InterruptedException e) {}
-
-            synchronized (lock2) {
-                System.out.println("Thread 1: Holding lock1 & lock2...");
-            }
-        }
-    }
-
-    public void method2() {
-        synchronized (lock2) {
-            System.out.println("Thread 2: Holding lock2...");
-            try { Thread.sleep(100); } catch (InterruptedException e) {}
-
-            synchronized (lock1) {
-                System.out.println("Thread 2: Holding lock2 & lock1...");
-            }
-        }
-    }
+  private static final Object lock1 = new Object();
+  private static final Object lock2 = new Object();
 
     public static void main(String[] args) {
         Dead_Lock deadlock = new Dead_Lock();
